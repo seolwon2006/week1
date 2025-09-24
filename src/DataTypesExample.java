06062a00089419db1509c0bbc88bd888dc33d390
Java <seolwon@gmail.com>
2025-09-24 14:36:26 +0900

    커밋

---
+++ src/DataTypesExample.java
@@ -0,0 +1,23 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int x = 10; // 기본형
+        int y = x; // 값 복사
+        y = 20; // y 값을 변경
+
+        String a = "hello";
+        String b = a;
+        b = "world";
+
+        int[] arr1 = {1, 2, 3}; // 참조형
+        int[] arr2 = arr1; // 주소 복사
+        arr2[0] = 100; // arr2를 통한 값 변경
+
+        System.out.println("x: " + x); // x는 10으로 유지
+        System.out.println("y: " + y); // y는 20으로 변경
+
+        System.out.println("a = " + a); // hello
+        System.out.println("b = " + b); // world
+
+        System.out.println("arr1[0]: " + arr1[0]); // arr1[0]도 100으로 변경 (arr1과 arr2는 같은 배열을 참조)
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        double b = 12.34;
+        boolean c = true;
+        char d = 'a';
+        String name = "John Doe";
+
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("" + a);
+        System.out.println("" + b);
+        System.out.println("" + c);
+        System.out.println("" + d);
+        System.out.println("" + name);
+        System.out.println("" + numbers);
+
+    }
+}
